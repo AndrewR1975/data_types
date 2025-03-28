cd421d9e40c9a5c90f047b434971337223665718
AndrewR1975 <andrew-romanov@yandex.ru>
2025-03-28 20:58:19 +0700

    add Task1 from HW 1

---
+++ Task1.java
@@ -0,0 +1,13 @@
+public class Task1 {
+    public static void main(String[] args){
+          System.out.println("\"Your time is limited\n\t so don't waste it\n\t\t living someone else's life\"\n\t\t\t Steve Jobs");
+/*
+        System.out.println("""
+                "Your time is limited,
+                    so don't waste it 
+                        living someone else's life"
+                            Steve Jobs""");
+
+ */
+                }
+}
